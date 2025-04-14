b765ed956c8d599d484d46acc7d05e4ad42cb7ed
alexbiviano <bivianoalex63@gmail.com>
2025-04-14 15:42:12 +0200

    [upd] first task completed

---
+++ names.js
@@ -0,0 +1,9 @@
+function createUser(firstName, lastName) {  
+    return {  
+        firstName: firstName,  
+        lastName: lastName  
+    };  
+}  
+
+// Esporta la funzione  
+module.exports = createUser;  
